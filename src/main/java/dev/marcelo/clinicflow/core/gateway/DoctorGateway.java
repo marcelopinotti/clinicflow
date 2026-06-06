3fb29ff2fa17a5f11882d5a3b43c6f4805949c5e
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-06-06 18:55:16 -0300

    feat:Expand DoctorGateway interface with CRUD methods for doctor management

--- src/main/java/dev/marcelo/clinicflow/core/gateway/DoctorGateway.java
+++ src/main/java/dev/marcelo/clinicflow/core/gateway/DoctorGateway.java
@@ -2,9 +2,19 @@ package dev.marcelo.clinicflow.core.gateway;
 
 import dev.marcelo.clinicflow.core.entities.Doctor;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface DoctorGateway {
 
     Doctor criarDoutor(Doctor doctor);
 
+    List<Doctor> listarDoutor();
+
+    Optional<Doctor> buscarDoutor(Long id);
+
+    Doctor atualizarDoutor(Doctor doctor);
+
+    void deleteDoutor(Long id);
 
 }
