f7abcc050fa86ae64ddc0c420979617c1d682391
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-06-08 12:36:14 -0300

    feat:Add Patient use cases to BeanConfig for patient management

--- src/main/java/dev/marcelo/clinicflow/infrastructure/beans/BeanConfig.java
+++ src/main/java/dev/marcelo/clinicflow/infrastructure/beans/BeanConfig.java
@@ -2,6 +2,7 @@ package dev.marcelo.clinicflow.infrastructure.beans;
 
 import dev.marcelo.clinicflow.core.entities.Doctor;
 import dev.marcelo.clinicflow.core.gateway.DoctorGateway;
+import dev.marcelo.clinicflow.core.gateway.PatientGateway;
 import dev.marcelo.clinicflow.core.usecases.doctor.AtualizarMedicoCase;
 import dev.marcelo.clinicflow.core.usecases.doctor.AtualizarMedicoCaseImpl;
 import dev.marcelo.clinicflow.core.usecases.doctor.BuscarMedicoCase;
@@ -12,6 +13,16 @@ import dev.marcelo.clinicflow.core.usecases.doctor.DeletarMedicoCase;
 import dev.marcelo.clinicflow.core.usecases.doctor.DeletarMedicoCaseImpl;
 import dev.marcelo.clinicflow.core.usecases.doctor.ListarMedicosCase;
 import dev.marcelo.clinicflow.core.usecases.doctor.ListarMedicosCaseImpl;
+import dev.marcelo.clinicflow.core.usecases.patient.AtualizarPacienteCase;
+import dev.marcelo.clinicflow.core.usecases.patient.AtualizarPacienteCaseImpl;
+import dev.marcelo.clinicflow.core.usecases.patient.BuscarPacienteCase;
+import dev.marcelo.clinicflow.core.usecases.patient.BuscarPacienteCaseImpl;
+import dev.marcelo.clinicflow.core.usecases.patient.CriarPacienteCase;
+import dev.marcelo.clinicflow.core.usecases.patient.CriarPacienteCaseImpl;
+import dev.marcelo.clinicflow.core.usecases.patient.DeletarPacienteCase;
+import dev.marcelo.clinicflow.core.usecases.patient.DeletarPacienteCaseImpl;
+import dev.marcelo.clinicflow.core.usecases.patient.ListarPacientesCase;
+import dev.marcelo.clinicflow.core.usecases.patient.ListarPacientesCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -45,6 +56,30 @@ public class BeanConfig {
         return new DeletarMedicoCaseImpl(doctorGateway);
     }
 
+    @Bean
+    public CriarPacienteCase criarPaciente(PatientGateway gateway){
+        return new CriarPacienteCaseImpl(gateway);
+    }
 
+    @Bean
+    public AtualizarPacienteCase atualizarPaciente(PatientGateway gateway) {
+        return new AtualizarPacienteCaseImpl(gateway);
+    }
+
+    @Bean
+    public ListarPacientesCase listarPacientes(PatientGateway gateway) {
+        return new ListarPacientesCaseImpl(gateway);
+    }
+
+    @Bean
+    public BuscarPacienteCase buscarPaciente(PatientGateway gateway) {
+        return new BuscarPacienteCaseImpl(gateway);
+    }
+
+
+    @Bean
+    public DeletarPacienteCase deletarPaciente(PatientGateway gateway) {
+        return new DeletarPacienteCaseImpl(gateway);
+    }
 
 }
