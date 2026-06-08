361015779afa8904fe9bd425333dc2ac55b2bfa3
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-06-08 12:36:01 -0300

    feat:Enhance PatientGateway interface with CRUD methods for patient management

--- src/main/java/dev/marcelo/clinicflow/core/gateway/PatientGateway.java
+++ src/main/java/dev/marcelo/clinicflow/core/gateway/PatientGateway.java
@@ -1,4 +1,19 @@
 package dev.marcelo.clinicflow.core.gateway;
 
+import dev.marcelo.clinicflow.core.entities.Patient;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PatientGateway {
+
+    Patient criarPaciente(Patient patient);
+
+    List<Patient> listarPacientes();
+
+    Optional<Patient> buscarPaciente(Long id);
+
+    Patient atualizarPaciente(Patient patient);
+
+    void deletePaciente(Long id);
 }
