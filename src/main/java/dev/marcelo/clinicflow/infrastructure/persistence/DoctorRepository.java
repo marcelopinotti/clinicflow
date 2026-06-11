9e9101ae76975320cd6aacff448567d2b12396e5
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-06-11 18:14:06 -0300

    feat: add method to find Doctors by clinic ID for enhanced querying

--- src/main/java/dev/marcelo/clinicflow/infrastructure/persistence/DoctorRepository.java
+++ src/main/java/dev/marcelo/clinicflow/infrastructure/persistence/DoctorRepository.java
@@ -2,5 +2,9 @@ package dev.marcelo.clinicflow.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DoctorRepository extends JpaRepository<DoctorEntity,Long> {
+
+    List<DoctorEntity> findByClinics_Id(Long clinicId);
 }
